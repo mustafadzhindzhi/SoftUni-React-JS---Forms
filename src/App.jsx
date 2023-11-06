7d125826a694f387cf819e2250b0ff2a703a740a
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-11-06 21:11:33 +0200

    asdasd

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ControlledForm from './components/ConrollerForm.jsx'
+// import UncontrolledForm from './components/UncontrolledForm.jsx'
+
+function App() {
+
+  return (
+    <>
+      <ControlledForm/>
+    </>
+  )
+}
+
+export default App
